11a7e0f203fbed36f0e80caf5e8819dcbdb4314b
kevinaubertlomellini <kevinaubert1@unm.edu>
2024-02-14 22:49:37 -0700

    Update crazyflie-examples

--- crazyflie/scripts/crazyflie_server.py
+++ crazyflie/scripts/crazyflie_server.py
@@ -20,6 +20,8 @@ import cflib.crtp
 from cflib.crazyflie.swarm import CachedCfFactory
 from cflib.crazyflie.swarm import Swarm
 from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.mem import MemoryElement
+from cflib.crazyflie.mem import Poly4D
 
 from crazyflie_interfaces.srv import Takeoff, Land, VelWorld, GoTo, RemoveLogging, AddLogging
 from crazyflie_interfaces.srv import UploadTrajectory, StartTrajectory, NotifySetpointsStop
@@ -824,15 +826,93 @@ class CrazyflieServer(Node):
         return response
 
     def _notify_setpoints_stop_callback(self, request, response, uri="all"):
-        self.get_logger().info("Notify setpoint stop not yet implemented")
+        self.get_logger().info(f"[{self.cf_dict[uri]}] Received notify setpoint stop")
+
+        if uri == "all":
+            for link_uri in self.uris:
+                self.swarm._cfs[link_uri].cf.commander.send_notify_setpoint_stop()
+        else:
+            self.swarm._cfs[uri].cf.commander.send_notify_setpoint_stop()
+
         return response
 
     def _upload_trajectory_callback(self, request, response, uri="all"):
-        self.get_logger().info("Notify trajectory not yet implemented")
+        id = request.trajectory_id
+        offset = request.piece_offset
+        lenght = len(request.pieces)
+        total_duration = 0
+        self.get_logger().info("[%s] upload_trajectory(id=%d,offset=%d, lenght=%d)" % (
+            self.cf_dict[uri],
+            id,
+            offset,
+            lenght,
+        ))
+
+        trajectory = []
+        for i in range(lenght):
+            piece = request.pieces[i]
+            px = Poly4D.Poly(piece.poly_x)
+            py = Poly4D.Poly(piece.poly_y)
+            pz = Poly4D.Poly(piece.poly_z)
+            pyaw = Poly4D.Poly(piece.poly_yaw)
+            duration = float(piece.duration.sec) + \
+                float(piece.duration.nanosec)/1e9
+            trajectory.append(Poly4D(duration, px, py, pz, pyaw))
+            total_duration = total_duration + duration
+
+        if uri == "all":
+            upload_success_all = True
+            for link_uri in self.uris:
+                trajectory_mem = self.swarm._cfs[link_uri].cf.mem.get_mems(
+                    MemoryElement.TYPE_TRAJ)[id]
+                trajectory_mem.trajectory = trajectory
+                upload_result = trajectory_mem.write_data_sync()
+                if not upload_result:
+                    self.get_logger().info(f"[{self.cf_dict[uri]}] Upload failed")
+                    upload_success_all = False
+                else:
+                    self.swarm._cfs[link_uri].cf.high_level_commander.define_trajectory(
+                        id, offset, len(trajectory))
+            if upload_success_all is False:
+                response.success = False
+                return response
+        else:
+            trajectory_mem = self.swarm._cfs[uri].cf.mem.get_mems(
+                MemoryElement.TYPE_TRAJ)[id]
+            trajectory_mem.trajectory = trajectory
+            upload_result = trajectory_mem.write_data_sync()
+            if not upload_result:
+                self.get_logger().info(f"[{self.cf_dict[uri]}] Upload failed")
+                response.success = False
+                return response
+            self.swarm._cfs[uri].cf.high_level_commander.define_trajectory(
+                id, offset, len(trajectory))
+
         return response
     
     def _start_trajectory_callback(self, request, response, uri="all"):
-        self.get_logger().info("Start trajectory not yet implemented")
+        id = request.trajectory_id
+        ts = request.timescale
+        rel = request.relative
+        rev = request.reversed
+        gm = request.group_mask
+
+        self.get_logger().info("[%s] start_trajectory(id=%d,timescale=%f,relative=%d, reversed=%d, group_mask=%d)" % (
+            self.cf_dict[uri],
+            id,
+            ts,
+            rel,
+            rev,
+            gm
+        ))
+        if uri == "all":
+            for link_uri in self.uris:
+                self.swarm._cfs[link_uri].cf.high_level_commander.start_trajectory(
+                    id, ts, rel, rev, gm)
+        else:
+            self.swarm._cfs[uri].cf.high_level_commander.start_trajectory(
+                id, ts, rel, rev, gm)
+
         return response
     
     def _poses_changed(self, msg):
